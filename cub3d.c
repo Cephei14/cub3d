e32c30245b5152273b721ffcef17b1757984e544
cephei14 <rafik.dhaibi@gmail.com>
2025-11-01 15:05:15 +0900

    update

--- cub3d.c
+++ cub3d.c
@@ -6,33 +6,99 @@
 /*   By: rdhaibi <rdhaibi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 15:50:45 by rdhaibi           #+#    #+#             */
-/*   Updated: 2025/10/31 13:45:42 by rdhaibi          ###   ########.fr       */
+/*   Updated: 2025/11/01 15:03:58 by rdhaibi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int map_check(char *str)
+int	wall_check(t_game *game, int n)
 {
-	return 0;
+	int	i;
+	int	j;
+
+	i = 0;
+	while (game->data[0][i] || game->data[n][i])
+	{
+		if (game->data[0][i] != '1' || game->data[n][i] != '1')
+			return (ft_printf("Error\nMap is not enclosed by walls\n"), FAIL);
+		i++;
+	}
+	i = 1;
+	while(game->data[i] && i < n)
+	{
+		j = ft_strlen(game->data[i]);
+		if (game->data[i][0] != '1' || game->data[i] != '1')
+			return (ft_printf("Error\nMap is not enclosed by walls\n"), FAIL);
+		i++;
+	}
+	return (SUCCESS);
+}
+
+int map_check(t_game *game, char *str)
+{
+	int n;
+	
+	while(game->data[n])
+		n++;
+	wall_check(str, n);
+	return SUCCESS;
 }
 
 int arg_check(char *str)
 {
-	return 0;
+	if(ft_strnstr(str, ".cub", 3))
+		return SUCCESS;
+	ft_printf("Only <.cub> files are supported\n");
+	return FAIL;
 }
+
+void txtr_init_NS(t_game *game)
+{
+	game->north_tex.img_ptr = NULL;
+	game->north_tex.addr = NULL;
+	game->north_tex.width = 0;
+	game->north_tex.height = 0;
+	game->north_tex.bpp = 0;
+	game->north_tex.line_len = 0;
+	game->north_tex.endian = 0;
+	
+	game->south_tex.img_ptr = NULL;
+	game->south_tex.addr = NULL;
+	game->south_tex.width = 0;
+	game->south_tex.height = 0;
+	game->south_tex.bpp = 0;
+	game->south_tex.line_len = 0;
+	game->south_tex.endian = 0;
+}
+void txtr_init_EW(t_game *game)
+{
+	game->east_tex.img_ptr = NULL;
+	game->east_tex.addr = NULL;
+	game->east_tex.width = 0;
+	game->east_tex.height = 0;
+	game->east_tex.bpp = 0;
+	game->east_tex.line_len = 0;
+	game->east_tex.endian = 0;
+	
+	game->west_tex.img_ptr = NULL;
+	game->west_tex.addr = NULL;
+	game->west_tex.width = 0;
+	game->west_tex.height = 0;
+	game->west_tex.bpp = 0;
+	game->west_tex.line_len = 0;
+	game->west_tex.endian = 0;
+	
+}
+
 
 void struct_init(t_game *game)
 {
 	game->mlx_ptr = NULL;
 	game->win_ptr = NULL;
-	game->img_first_pixel = NULL;
+	game->img_pixel = NULL;
 	game->img_ptr = NULL;
 	game->data = NULL;
-	game->no = NULL;
-	game->so = NULL;
-	game->ea = NULL;
-	game->we = NULL;
 	game->floor_color = 0;
 	game->ceiling_color = 0;
 	game->bpp = 0;
@@ -47,29 +113,67 @@ void struct_init(t_game *game)
 	game->dir_y = 0.0;
 	game->x_plane = 0.0;
 	game->y_plane = 0.0;
+	txtr_init_NS(&game);
+	txtr_init_EW(&game);
+}
+
+int exit_game(t_game *game)
+{
+	mlx_destroy_window(game->mlx_ptr, game->win_ptr); //destroys the window
+	exit(0);
+}
+
+int textures(t_game *game)
+{
+	int width;
+	int height;
+	
+	mlx_xpm_file_to_image();
+}
+
+int ray_casting(t_game *game)
+{
+	mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->img_ptr, 0, 0); //Adjusts (0, 0) of canvas with (0, 0) with the image
+}
+
+void exit_game(t_game *game)
+{
+	mlx_destroy_window(game->mlx_ptr, game->win_ptr); //destroys the window
+	exit(0);
 }
 
 int main(int ac, char **av)
 {
 	t_game game;
 	
-	if(av != 2)
+	if(ac != 2)
 	{
 		ft_printf("Use ./cub3d <map's path>\n");
-		return 1;
+		return FAIL;
 	}
 	if(arg_check(av[1]) == 1 || map_check(av[1]) == 1)
-		return 1;
+		return FAIL;
 	struct_init(&game);
-	if ((game.mlx_ptr = mlx_init()) == NULL)
-		return 1;
-	if((game.win_ptr = mlx_new_window(game.mlx_ptr, WIDTH, HEIGHT, "Cube 3D")) == NULL)
-		return 1;
-	if ((game.img_ptr = mlx_new_image(game.mlx_ptr, WIDTH, HEIGHT)) == NULL)
-		return 1;
+	if ((game.mlx_ptr = mlx_init()) == NULL) //connects the computer's graphics with my code
+		return FAIL;
+		
+	if((game.win_ptr = mlx_new_window(game.mlx_ptr, WIDTH, HEIGHT, "Cube 3D")) == NULL) //opens a new window and give a title
+		return FAIL;
+		
+	if ((game.img_ptr = mlx_new_image(game.mlx_ptr, WIDTH, HEIGHT)) == NULL) //creates a "canvas" to start drawing on it
+		return FAIL;
+		
+	if ((game.img_pixel = mlx_get_data_addr(game.img_ptr, 
+		&game.bpp, &game.line_len, &game.endian)) == NULL) //you give it the canvas and other informations's adrresses so it fill those informations and return a pointer of the first pixel of the image
+		return FAIL;											//this one is for the screen
+		
+	if (textures(&game) == 1) //upload textures...
+		return FAIL;
+		
+	if (ray_casting(&game) == 1) //draw pixels
+		return FAIL;
 	
-
-
-	
-	mlx_destroy_window(game.mlx_ptr, game.win_ptr);
+	mlx_loop(game.mlx_ptr); //infinite loop, it keep listening to event...
+	exit_game(&game);
+	return SUCCESS;
 }
--- cub3d.h
+++ cub3d.h
@@ -1,58 +1,81 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.h                                             :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rdhaibi <rdhaibi@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 15:50:41 by rdhaibi           #+#    #+#             */
-/*   Updated: 2025/10/31 12:08:12 by rdhaibi          ###   ########.fr       */
+/*   Updated: 2025/11/01 14:50:53 by rdhaibi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
 # include <stdlib.h>
 # include <stdio.h>
 # include <fcntl.h>
+# include "ft_printf/ft_printf.h"
 # include "libft/libft.h"
 # include "get_next_line/get_next_line.h"
 # include "minilibx-linux/mlx.h"
 
 # define WIDTH 800
 # define HEIGHT 600
+# define SUCCESS 0
+# define FAIL 0
+
+typedef struct s_texture
+{
+    void    *img_ptr; //Image's ID
+    char    *addr;    //Address of the first pixel
+    int     width;    //Width of the texture
+    int     height;   //Height of the texture
+    int     bpp;      //Bits per pixel
+    int     line_len; //Number of bytes in one horizontal row of the image
+    int     endian;   //(RGB) Byte's order
+}   t_texture;
 
 typedef struct s_game
 {
-	//mlx's data
-	void *mlx_ptr;
-	void *win_ptr;
-	//map's data
-	char **data;
-	char *no;
-	char *so;
-	char *ea;
-	char *we;
-	int floor_color;
-	int ceiling_color;
-	//screen image's data
-	void *img_ptr;
-	char *img_first_pixel;
-	int bpp;
-	int line_len;
-	int endian;
-	//player's data
-	char start_dir;
-	int start_x;
-	int start_y;
-	double pos_x;
-	double pos_y;
-	double dir_x;
-	double dir_y;
-	double x_plane;
-	double y_plane;
-	
+    //mlx's data
+    void *mlx_ptr;    //Connection's ID between projects and computer's graphics
+    void *win_ptr;    //Window's ID
+    
+    //map's data
+    char **data;      //Map's grid
+    char *no_path;    //File's path to textures .xpm
+    char *so_path;    //File's path to textures .xpm
+    char *ea_path;    //File's path to textures .xpm
+    char *we_path;    //File's path to textures .xpm
+    int floor_color;  //Color as a single integer
+    int ceiling_color;//Color as a single integer
+
+    //screen image's data
+    void *img_ptr;    //Blank canvas's ID
+    char *img_pixel;  //Address of the first pixel of your canvas
+    int bpp;          //Canvas's property : bits per pixel
+    int line_len;     //Canvas's property : line length
+    int endian;       //Canvas's property : (RGB) Byte's order
+    
+    //player's data
+    char start_dir;   //Start direction (N,S,E,W)
+    int start_x;      //Grid coordinates X (X,Y)
+    int start_y;      //Grid coordinates Y (X,Y)
+    double pos_x;     //Player's current X position 
+    double pos_y;     //Player's current Y position 
+    double dir_x;     //Player's current X direction 
+    double dir_y;     //Player's current Y direction
+    double x_plane;   //Camera's X plane vector(for raycasting).
+    double y_plane;   //Camera's Y plane vector(for raycasting).
+
+    //textures's data
+    t_texture north_tex;
+    t_texture south_tex;
+    t_texture east_tex;
+    t_texture west_tex;
+
 } t_game;
 
 #endif
